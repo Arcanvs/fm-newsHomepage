5a1f7161ed85c9cd7b014a83e32f09b7059f89b2
Jose <jose.osorio.herrera@gmail.com>
2023-01-26 00:40:50 -0300

    First commit :metal:

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <main className="App">
+      News Home Page <img src={logo} className="logo W" alt="Logo News HomePage" />
+    </main>
+  )
+}
+
+export default App
