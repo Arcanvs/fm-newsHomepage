57bc5cc4b0b1e85aa5d67770ce60694bcd277461
Jose <jose.osorio.herrera@gmail.com>
2023-01-28 04:07:22 -0300

    fix ruta imgs

--- src/components/organisms/PostList.jsx
+++ src/components/organisms/PostList.jsx
@@ -5,17 +5,17 @@ const PostList = () => {
         numberTitle: '01', 
         textTitle: 'Reviving Retro PCs', 
         textContent : 'What happens when old PCs are given modern upgrades?', 
-        imgUrl: '/images/image-retro-pcs.jpg'
+        imgUrl: './images/image-retro-pcs.jpg'
     },{
         numberTitle: '02', 
         textTitle: 'Top 10 Laptops of 2022', 
         textContent : 'Our best picks for various needs and budgets.', 
-        imgUrl: '/images/image-top-laptops.jpg'
+        imgUrl: './images/image-top-laptops.jpg'
     },{
         numberTitle: '03', 
         textTitle: 'The Growth of Gaming', 
         textContent : 'How the pandemic has sparked fresh opportunities.', 
-        imgUrl: '/images/image-gaming-growth.jpg'
+        imgUrl: './images/image-gaming-growth.jpg'
     }];
   return (
     <>
